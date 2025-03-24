b37180f4d17e813b8f8d0c29e0cc0ebfba723403
georgekibe <georgekibe500@gmail.com>
2025-03-24 23:24:06 +0300

    Reversed words in given string
    Code updated for this

--- src/main/java/ReversingWords.java
+++ src/main/java/ReversingWords.java
@@ -1,13 +1,28 @@
+
+/*
+ * Reversed words in given string
+ * */
+
+
 public class ReversingWords {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         String input = "George Kibe";
 
-        StringBuilder
-    }
+        String[] words = input.split(" ");
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            output.append(words[i]).append(" ");
+        }
+
+
+        System.out.println(output);
 
     }
+
+}
 
 
